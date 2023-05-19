9b05269e604ef040011dc7b35973648598bd3191
ben little <ben.little@benlittle.dev>
2023-05-19 14:54:59 -0600

    income and expense summaries working

--- src/app.rs
+++ src/app.rs
@@ -7,19 +7,25 @@ use yew::prelude::*;
 pub fn app() -> Html {
     let state_handle = use_state_eq(State::new);
     let state = (*state_handle).clone();
-    let submit_income = {
-        let state = state_handle.clone();
-        Callback::from(move |entry: IncomeFormEntry| {
-            let mut new_state = (*state).clone();
-            new_state.submit_income_form(entry);
-            state.set(new_state)
-        })
-    };
     html! {
         <main>
             <Timeline dates={state.dates()} />
-            <IncomeForm submit={submit_income}/>
-            <ExpenseForm />
+            <IncomeForm submit={
+                let state = state_handle.clone();
+                move |entry: IncomeFormEntry| {
+                    let mut new_state = (*state).clone();
+                    new_state.submit_income_form(entry);
+                    state.set(new_state)
+                }
+            }/>
+            <ExpenseForm submit={
+                let state = state_handle.clone();
+                move |entry: ExpenseFormEntry| {
+                    let mut new_state = (*state).clone();
+                    new_state.submit_expense_form(entry);
+                    state.set(new_state)
+                }
+            }/>
         </main>
     }
 }
@@ -34,7 +40,9 @@ pub fn timeline(props: &TimelineProps) -> Html {
     fn date_to_html(date: &Date) -> Html {
         html! {
             <li>
-                <p>{"Date"}</p>
+                <p>{date.date.clone()}</p>
+                <p>{"Income: "}{date.total_income()}</p>
+                <p>{"Expenses: "}{date.total_expenses()}</p>
             </li>
         }
     }
@@ -59,57 +67,127 @@ pub fn income_form(props: &IncomeFormProps) -> Html {
     let date_handle = use_state(String::default);
     let value = (*value_handle).clone();
     let date = (*date_handle).clone();
+
+    let submit = {
+        let value_handle = value_handle.clone();
+        let date_handle = date_handle.clone();
+        let submit_callback = props.submit.clone();
+        let value = (*value_handle).clone();
+        move |_| match value.parse::<u32>() {
+            Ok(n) => {
+                submit_callback.emit(IncomeFormEntry {
+                    value: n,
+                    date: (*date_handle).clone(),
+                });
+            }
+            Err(e) => gloo_console::log!("{e:?}"),
+        }
+    };
+
+    let on_value_change = {
+        let value_handle = value_handle.clone();
+        move |e: Event| {
+            if let Some(input) = e
+                .target()
+                .and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
+            {
+                value_handle.set(input.value());
+            }
+        }
+    };
+
+    let on_date_change = {
+        let date_handle = date_handle.clone();
+        move |e: Event| {
+            if let Some(input) = e
+                .target()
+                .and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
+            {
+                date_handle.set(input.value());
+            }
+        }
+    };
+
     html! {
         <section>
             <p>{"Income Form"}</p>
-            <input onchange={
-                let value_handle = value_handle.clone();
-                move |e: Event| {
-                    if let Some(input) = e
-                        .target()
-                        .and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
-                    {
-                        value_handle.set(input.value());
-                    }
-                }
-            }
+            <input onchange={on_value_change}
                 type="text"
                 value={value}
             />
-            <input onchange={
-                let date_handle = date_handle.clone();
-                move |e: Event| {
-                    if let Some(input) = e
-                        .target()
-                        .and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
-                    {
-                        date_handle.set(input.value());
-                    }
-                }
-            }
+            <input onchange={on_date_change}
                 type="date"
                 value={date}
             />
-            <button onclick={
-                let value_handle = value_handle.clone();
-                let date_handle = date_handle.clone();
-                let submit_callback = props.submit.clone();
-                move |_| {
-                    submit_callback.emit(IncomeFormEntry {
-                        value: (*value_handle).clone(),
-                        date: (*date_handle).clone()
-                    })
-                }
-            }>{"Submit"}</button>
+            <button onclick={submit}>{"Submit"}</button>
         </section>
     }
 }
 
+#[derive(Properties, PartialEq)]
+pub struct ExpenseFormProps {
+    pub submit: Callback<ExpenseFormEntry>,
+}
+
 #[function_component(ExpenseForm)]
-pub fn expense_form() -> Html {
+pub fn expense_form(props: &ExpenseFormProps) -> Html {
+    let value_handle = use_state(String::default);
+    let date_handle = use_state(String::default);
+    let value = (*value_handle).clone();
+    let date = (*date_handle).clone();
+
+    let submit = {
+        let value_handle = value_handle.clone();
+        let date_handle = date_handle.clone();
+        let submit_callback = props.submit.clone();
+        let value = (*value_handle).clone();
+        move |_| match value.parse::<u32>() {
+            Ok(n) => {
+                submit_callback.emit(ExpenseFormEntry {
+                    value: n,
+                    date: (*date_handle).clone(),
+                });
+            }
+            Err(e) => gloo_console::log!("{e:?}"),
+        }
+    };
+
+    let on_value_change = {
+        let value_handle = value_handle.clone();
+        move |e: Event| {
+            if let Some(input) = e
+                .target()
+                .and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
+            {
+                value_handle.set(input.value());
+            }
+        }
+    };
+
+    let on_date_change = {
+        let date_handle = date_handle.clone();
+        move |e: Event| {
+            if let Some(input) = e
+                .target()
+                .and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
+            {
+                date_handle.set(input.value());
+            }
+        }
+    };
+
     html! {
         <section>
             <p>{"Expense Form"}</p>
+            <input onchange={on_value_change}
+                type="text"
+                value={value}
+            />
+            <input onchange={on_date_change}
+                type="date"
+                value={date}
+            />
+            <button onclick={submit}>{"Submit"}</button>
         </section>
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -1,29 +1,76 @@
+use std::{collections::BTreeMap, vec};
+
 use wasm_bindgen::JsValue;
 
 #[derive(Clone, PartialEq)]
-pub struct State {}
+pub struct State {
+    pub dates: BTreeMap<String, (Vec<IncomeFormEntry>, Vec<ExpenseFormEntry>)>,
+}
 
 impl State {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            dates: BTreeMap::new(),
+        }
     }
-    
+
     pub fn dates(&self) -> Vec<Date> {
-        vec![Date{}, Date{}, Date{}]
+        let mut dates = Vec::new();
+        for (date, (income_entries, expense_entries)) in &self.dates {
+            dates.push(Date {
+                date: date.clone(),
+                income_entries: income_entries.clone(),
+                expense_entries: expense_entries.clone(),
+            })
+        }
+        dates
     }
-    
+
     pub fn submit_income_form(&mut self, entry: IncomeFormEntry) {
         let msg = format!("income form entry: {entry:?}");
-        gloo_console::log!(JsValue::from(msg))
+        gloo_console::log!(JsValue::from(msg));
+        if let Some((income_entries, _)) = self.dates.get_mut(&entry.date) {
+            income_entries.push(entry)
+        } else {
+            self.dates.insert(entry.date.clone(), (vec![entry], vec![]));
+        };
     }
 
+    pub fn submit_expense_form(&mut self, entry: ExpenseFormEntry) {
+        let msg = format!("expense form entry: {entry:?}");
+        gloo_console::log!(JsValue::from(msg));
+        if let Some((_, expense_entries)) = self.dates.get_mut(&entry.date) {
+            expense_entries.push(entry)
+        } else {
+            self.dates.insert(entry.date.clone(), (vec![], vec![entry]));
+        };
+    }
 }
 
-#[derive(PartialEq)]
-pub struct Date {}
+#[derive(PartialEq, Clone)]
+pub struct Date {
+    pub date: String,
+    pub income_entries: Vec<IncomeFormEntry>,
+    pub expense_entries: Vec<ExpenseFormEntry>,
+}
 
-#[derive(Debug)]
+impl Date {
+    pub fn total_income(&self) -> u32 {
+        self.income_entries.iter().map(|a| a.value).sum()
+    }
+    pub fn total_expenses(&self) -> u32 {
+        self.expense_entries.iter().map(|a| a.value).sum()
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
 pub struct IncomeFormEntry {
-    pub value: String,
-    pub date: String
+    pub value: u32,
+    pub date: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ExpenseFormEntry {
+    pub value: u32,
+    pub date: String,
 }
