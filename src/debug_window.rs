745c25396bd3d68bc32c4793bdbf0d42a7767cbc
ben little <ben.little@benlittle.dev>
2023-05-26 08:03:03 -0600

    enable hiding debug info

--- src/debug_window.rs
+++ src/debug_window.rs
@@ -8,36 +8,69 @@ pub struct DebugWindowProps {
 
 #[function_component(DebugWindow)]
 pub fn debug_window(props: &DebugWindowProps) -> Html {
+    let show = use_state(|| false);
+    let onclick = {
+        let show = show.clone();
+        move |_| {
+            show.set(!(*show));
+        }
+    };
     html! {
         <>
         <p><b>{"Debug Info:"}</b></p>
-        <p>{"Log Entries:"}</p>
-        {for props.log.iter().map(|e: &Entry| match e {
-            Entry::Create(t) => html!{
-                <>
-                <hr />
-                <p>{"Create"}</p>
-                <p>{"Date: "}{t.date}</p>
-                <p>{"Kind: "}{t.kind}</p>
-                <p>{"Value: "}{t.value}</p>
-                </>
-            },
-            Entry::Delete(id) => html! {
-                <>
-                <hr />
-                <p>{"Delete"}</p>
-                <p>{"Id: "}{id}</p>
-                </>
-            },
-            Entry::SetDate(date_range) => html!{
-                <>
-                <hr />
-                <p>{"Set Date Range"}</p>
-                <p>{"Start: "}{date_range.start}</p>
-                <p>{"End: "}{date_range.end}</p>
-                </>
+        <button {onclick}>{
+            if *show {
+                "Hide debug info"
+            } else {
+                "Show debug info"
             }
-        })}
+        }</button>
+        {if *show {
+            html!{<LogEntries log={props.log.clone()} />}
+        } else {
+            html!{}
+        }}
         </>
+    }
+}
+
+
+#[derive(Properties, PartialEq)]
+struct LogEntriesProps {
+    pub log: Vec<Entry>,
+}
+
+#[function_component(LogEntries)]
+fn log_entries(props: &LogEntriesProps) -> Html {
+    html!{
+    <>
+    <p>{"Log Entries:"}</p>
+    {for props.log.iter().map(|e: &Entry| match e {
+        Entry::Create(t) => html!{
+            <>
+            <hr />
+            <p>{"Create"}</p>
+            <p>{"Date: "}{t.date}</p>
+            <p>{"Kind: "}{t.kind}</p>
+            <p>{"Value: "}{t.value}</p>
+            </>
+        },
+        Entry::Delete(id) => html! {
+            <>
+            <hr />
+            <p>{"Delete"}</p>
+            <p>{"Id: "}{id}</p>
+            </>
+        },
+        Entry::SetDate(date_range) => html!{
+            <>
+            <hr />
+            <p>{"Set Date Range"}</p>
+            <p>{"Start: "}{date_range.start}</p>
+            <p>{"End: "}{date_range.end}</p>
+            </>
+        }
+    })}
+    </>
     }
 }
